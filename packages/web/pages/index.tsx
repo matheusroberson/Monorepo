a00e08dc567d9c27065576141b41f0b0d5e31d71
RGTS <mroberson115@gmail.com>
2021-08-04 13:05:23 -0300

    Limpando arquivos

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,46 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native'
-import Demo from "../../shared/components/Demo"
-import Link from "../../shared/components/Link"
+import { Text, View } from 'react-native'
 
 export default function App(props) {
   return (
-    <View style={styles.container}>
-      <Text accessibilityRole="header" style={styles.text}>
+    <View>
+      <Text accessibilityRole="header">
         React Native for Web & Next.js
       </Text>
-
-      <Link route="/alternate" componentType="web">
-        <Text style={styles.link}>
-          A universal link
-        </Text>
-      </Link>
-
-      <View style={styles.textContainer}>
-        <Text accessibilityRole="header" aria-level="2" style={styles.text}>
-          Subheader
-        </Text>
-      </View>
-      <Demo/>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    flexGrow: 1,
-    justifyContent: 'center',
-  },
-  link: {
-    color: 'blue',
-  },
-  textContainer: {
-    alignItems: 'center',
-    marginTop: 16,
-  },
-  text: {
-    alignItems: 'center',
-    fontSize: 24,
-    marginBottom: 24,
-  },
-})
