962c2496a8c50da55e5b7a8aa09cd74065772ca8
RGTS <mroberson115@gmail.com>
2021-08-08 18:55:19 -0300

    Configurando variáveis de ambiente - Android

--- packages/shared/api/index.ts
+++ packages/shared/api/index.ts
@@ -1,5 +1,8 @@
+import Config from 'react-native-config'
+const TOKEN_API = process.env.TOKEN_API || Config.TOKEN_API
+
 export const getInfosSymbol = (symbol: string) => {
-    return fetch(`https://sandbox.iexapis.com/stable/stock/${symbol}/quote/?token=${process.env.TOKEN_API}`, {
+    return fetch(`https://sandbox.iexapis.com/stable/stock/${symbol}/quote/?token=${TOKEN_API}`, {
         method: 'GET',
         headers: {
             Accept: 'application/json',
