f9201319282fbd80a543935a8e5e151bef8bc369
RGTS <mroberson115@gmail.com>
2021-08-09 10:48:33 -0300

    Add secção profile

---
+++ packages/web/components/Profile/index.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { TouchableOpacity, View, Image, Text, StyleSheet } from "react-native";
+
+const Profile = () => {
+  return (
+    <View style={styles.section}>
+      <TouchableOpacity style={styles.profile}>
+        <Image
+          style={styles.image}
+          source={{ uri: "https://i.imgur.com/9w1I68p.png" }}
+          resizeMode={"cover"}
+        />
+        <Text style={styles.name}>João da Silva Almeida Magalhães</Text>
+        <Text style={styles.arrow}></Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  section: {
+    alignItems: "center",
+    width: "100%",
+  },
+  profile: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 4,
+    border: "1px solid rgba(0, 71, 187, 0.2)",
+    boxSizing: "border-box",
+    borderRadius: 120,
+    height: 40,
+    width: 346,
+  },
+  name: {
+    color: "#0047BB",
+    fontFamily: "Graphik",
+    fontStyle: "normal",
+    fontWeight: "500",
+    fontSize: 16,
+    lineHeight: 20,
+    textAlign: "center",
+    letterSpacing: -0.005,
+  },
+  image: {
+    width: 32,
+    height: 32,
+    backgroundColor: "#0047BB",
+    borderRadius: 100,
+  },
+  arrow: {
+    backgroundColor: "#FFF",
+    borderRadius: 1,
+    width: 12,
+    height: 6,
+    marginRight: 10,
+    borderTopColor: "#F06400",
+    borderLeftColor: "transparent",
+    borderRightColor: "transparent",
+    borderTopWidth: 6,
+    borderLeftWidth: 6,
+    borderRightWidth: 6,
+    borderStyle: "solid",
+  },
+});
+
+export default Profile;
--- packages/web/pages/dashboard.tsx
+++ packages/web/pages/dashboard.tsx
@@ -1,19 +1,20 @@
 import React from "react";
-import { StyleSheet, View, Dimensions } from "react-native";
-import {
-  Container,
-  Input,
-  Button,
-  Text,
-} from "../../shared/components/Home/styles";
+import { StyleSheet, View, Text } from "react-native";
+import { Container, Input, Button } from "../../shared/components/Home/styles";
 import Sidebar from "../components/Sidebar";
+import Profile from "../components/Profile";
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Sidebar styles={styles.anotherblock} />
-      <View style={styles.block}>
-        <Text>Teste</Text>
+      <Sidebar styles={styles.sidebar} />
+      <View style={styles.mainSection}>
+        <View style={styles.dashSection}>
+          <Text>Block A</Text>
+        </View>
+        <View style={styles.profileSection}>
+          <Profile />
+        </View>
       </View>
     </View>
   );
@@ -26,16 +27,34 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
     alignItems: "flex-start",
     width: "100%",
-    // backgroundColor: "blue",
+    backgroundColor: "#FFFFFF",
   },
-  block: {
-    // backgroundColor: "purple",
+  mainSection: {
+    flex: 1,
+    flexDirection: "row",
     height: "100%",
+    width: "100%",
+    borderTopLeftRadius: 24,
+    backgroundColor: "#C4C4C4",
   },
-  anotherblock: {
+  sidebar: {
     width: 120,
     height: "100%",
     backgroundColor: "#FFFFFF",
+  },
+  dashSection: {
+    backgroundColor: "#C4C4C4",
+    borderTopLeftRadius: 24,
+    width: "66%",
+  },
+  profileSection: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    paddingVertical: 32,
+    paddingHorizontal: 20,
+    backgroundColor: "#FFFFFF",
+    width: "34%",
   },
 });
 
