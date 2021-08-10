c94584c17d4e680510b71c6bfff3d3bda37dd6a3
RGTS <mroberson115@gmail.com>
2021-08-10 00:13:55 -0300

    Add route na sidebar

--- packages/web/components/Sidebar/index.tsx
+++ packages/web/components/Sidebar/index.tsx
@@ -2,27 +2,49 @@ import React from "react";
 import { StyleSheet, ScrollView, View, Text } from "react-native";
 import Logo from "../../../shared/components/Icons/Logo";
 import Main from "../../../shared/components/Icons/Main";
+import Link from "next/link";
+import { useRouter } from "next/router";
 interface Props {
   styles: {};
 }
 
 const App = (props: Props) => {
-  const onItemSelected = (item: string) => {};
-
+  const router = useRouter();
   return (
     <View style={props.styles}>
       <View>
         <View style={styles.sidebarLogo}>
-          <Logo width={46} height={46} />
+          <Link href="/">
+            <Logo width={46} height={46} />
+          </Link>
         </View>
       </View>
       <ScrollView scrollsToTop={false} style={styles.menu}>
-        <View style={styles.menuContainer}>
-          <Text style={styles.item}></Text>
-          <Text onPress={() => onItemSelected("About")}>
-            <Main width={32} height={32} fill={"#0047BB"} stroke={"#0047BB"} />
-          </Text>
-        </View>
+        {router.route !== "/" ? (
+          <View style={styles.menuContainer}>
+            <Text style={styles.item}></Text>
+            <Text>
+              <Main
+                width={32}
+                height={32}
+                fill={"#0047BB"}
+                stroke={"#0047BB"}
+              />
+            </Text>
+          </View>
+        ) : (
+          <View style={styles.menuContainer}>
+            <Text style={styles.itemDesactive}></Text>
+            <Text>
+              <Main
+                width={32}
+                height={32}
+                fill={"#0047BB"}
+                stroke={"#0047BB"}
+              />
+            </Text>
+          </View>
+        )}
       </ScrollView>
     </View>
   );
@@ -33,9 +55,18 @@ const styles = StyleSheet.create({
     marginTop: 20,
     justifyContent: "center",
     alignItems: "center",
+    cursor: "pointer",
   },
   item: {
     backgroundColor: "#F06400",
+    borderTopRightRadius: 20,
+    borderBottomRightRadius: 20,
+    marginRight: 32,
+    height: 48,
+    width: 4,
+  },
+  itemDesactive: {
+    backgroundColor: "#A9ADB1",
     borderTopRightRadius: 20,
     borderBottomRightRadius: 20,
     marginRight: 32,
