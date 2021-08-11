e030dbd5ac8b785079577d588bc051c35027a7ce
RGTS <mroberson115@gmail.com>
2021-08-11 13:06:34 -0300

    Api assíncrona

--- packages/shared/api/index.ts
+++ packages/shared/api/index.ts
@@ -1,16 +1,14 @@
 import Config from 'react-native-config'
 const TOKEN_API = process.env.TOKEN_API || Config.TOKEN_API
 
-export const getInfosSymbol = (symbol: string) => {
-    return fetch(`https://sandbox.iexapis.com/stable/stock/${symbol}/quote/?token=${TOKEN_API}`, {
+export const getInfosSymbol = async (symbol: string) => {
+    const response = await fetch(`https://sandbox.iexapis.com/stable/stock/${symbol}/quote/?token=${TOKEN_API}`, {
         method: 'GET',
         headers: {
             Accept: 'application/json',
             'Content-Type': 'application/json'
         },
     })
-    .then(response => { return response.json() })
-    .then((res) => {
-        return { symbol: symbol.toUpperCase(), companyName: res.companyName, change: res.change, latestPrice: res.latestPrice }
-    })
+    const res = await response.json()
+    return { symbol: symbol.toUpperCase(), companyName: res.companyName, change: res.change, latestPrice: res.latestPrice }
 } 
