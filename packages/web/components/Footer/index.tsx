930c3ab1a3fd5b0816fb9c59b8f4ee0e1248d588
RGTS <mroberson115@gmail.com>
2021-08-13 18:10:21 -0300

    Add navegação no footer

--- packages/web/components/Footer/index.tsx
+++ packages/web/components/Footer/index.tsx
@@ -4,9 +4,10 @@ import {
   TouchableOpacity,
   Text,
   StyleSheet,
-  SafeAreaView,
+  FlatList,
   ScrollView,
   Image,
+  Dimensions,
 } from "react-native";
 import Star from "../../../shared/components/Icons/Star";
 import Grow from "../../../shared/components/Icons/Grow";
@@ -18,6 +19,7 @@ import {
   addFavorites,
   removeFavorites,
   selectInfos,
+  infosSymbol,
 } from "../../store/infos/infosSlice";
 interface footerProps {
   history: {
@@ -32,6 +34,7 @@ const Footer = (props: footerProps) => {
   const ref = React.useRef(null);
   const dispatch = useAppDispatch();
   const { favorites } = useAppSelector(selectInfos);
+  const [count, setCount] = React.useState(0);
 
   const CustomStar = (fill, stroke, action, symbol) => {
     return (
@@ -47,6 +50,88 @@ const Footer = (props: footerProps) => {
     );
   };
 
+  const renderHistory = (value, key) => {
+    const perChange =
+      ((value.latestPrice - (value.latestPrice - value.change)) /
+        value.latestPrice) *
+      100;
+    const uri = symbolToLogo(value.symbol.toLowerCase());
+
+    return (
+      <View style={styles.containerBlockCard} key={key}>
+        <View style={styles.card}>
+          {favorites.length !== 0
+            ? favorites.filter((valueFav) =>
+                valueFav.symbol.includes(value.symbol)
+              ).length !== 0
+              ? CustomStar("#0047BB", "#0047BB", removeFavorites, value.symbol)
+              : CustomStar("#FFF", "#0047BB", addFavorites, value.symbol)
+            : CustomStar("#FFF", "#0047BB", addFavorites, value.symbol)}
+          <TouchableOpacity
+            onPress={() => {
+              dispatch(infosSymbol(value.symbol.toLowerCase()));
+            }}
+            style={styles.containerInfo}
+          >
+            <Image style={styles.image} source={{ uri }} resizeMode={"cover"} />
+            <View style={styles.cardCompany}>
+              <Text style={styles.symbol}>{value.symbol}</Text>
+              <Text style={styles.companyName}>
+                {value.companyName.split(" ")[0]}
+              </Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              dispatch(infosSymbol(value.symbol.toLowerCase()));
+            }}
+          >
+            {perChange > 0 ? (
+              <Text style={[styles.changePositive, styles.change]}>
+                {perChange.toFixed(2)}%
+                <Grow
+                  width={18}
+                  height={18}
+                  stroke={"#79C300"}
+                  transform={"translate(0 4)"}
+                />
+              </Text>
+            ) : (
+              <Text style={[styles.changeNegative, styles.change]}>
+                {perChange.toFixed(2)}%
+                <Grow
+                  transform={"rotate(180) scale(-1, 1) translate(0 -4)"}
+                  width={18}
+                  height={18}
+                  stroke={"#D64B45"}
+                />
+              </Text>
+            )}
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
+
+  const calcPoints = (commd: string) => {
+    let points = [];
+    const xTimes = parseFloat(
+      (ref.current.scrollWidth / ref.current.clientWidth).toFixed(1).toString()
+    );
+    for (let i = 0; i < xTimes; i++) {
+      points.push(ref.current.clientWidth * i);
+    }
+
+    if (commd === "next") {
+      if (count < points.length - 1) {
+        setCount(count + 1);
+      }
+    } else if (commd === "prev") {
+      setCount(0);
+    }
+
+    ref.current.scrollTo({ animated: true, x: points[count] });
+  };
   return (
     <View style={styles.containerFooter}>
       <View style={styles.containerFooterHeader}>
@@ -58,101 +143,37 @@ const Footer = (props: footerProps) => {
           <TouchableOpacity
             style={[styles.controls, styles.controlsLeft]}
             onPress={() => {
-              ref.current.scrollTo({ x: -500, animated: true });
+              calcPoints("prev");
             }}
           ></TouchableOpacity>
           <TouchableOpacity
             style={[styles.controls, styles.controlsRight]}
             onPress={() => {
-              ref.current.scrollTo({ x: 500, animated: true });
+              calcPoints("next");
             }}
           ></TouchableOpacity>
         </View>
       </View>
       <View>
-        <SafeAreaView>
+        <>
           <ScrollView
-            horizontal
             showsHorizontalScrollIndicator={false}
+            horizontal
             ref={ref}
             style={styles.slide}
           >
-            {props.history.map((value, key) => {
-              const perChange =
-                ((value.latestPrice - (value.latestPrice - value.change)) /
-                  value.latestPrice) *
-                100;
-              const uri = symbolToLogo(value.symbol.toLowerCase());
-
-              return (
-                <View style={styles.containerBlockCard} key={key}>
-                  <View style={styles.card}>
-                    {favorites.length !== 0
-                      ? favorites.filter((valueFav) =>
-                          valueFav.symbol.includes(value.symbol)
-                        ).length !== 0
-                        ? CustomStar(
-                            "#0047BB",
-                            "#0047BB",
-                            removeFavorites,
-                            value.symbol
-                          )
-                        : CustomStar(
-                            "#FFF",
-                            "#0047BB",
-                            addFavorites,
-                            value.symbol
-                          )
-                      : CustomStar(
-                          "#FFF",
-                          "#0047BB",
-                          addFavorites,
-                          value.symbol
-                        )}
-                    <TouchableOpacity style={styles.containerInfo}>
-                      <Image
-                        style={styles.image}
-                        source={{ uri }}
-                        resizeMode={"cover"}
-                      />
-                      <View style={styles.cardCompany}>
-                        <Text style={styles.symbol}>{value.symbol}</Text>
-                        <Text style={styles.companyName}>
-                          {value.companyName}
-                        </Text>
-                      </View>
-                    </TouchableOpacity>
-                    <TouchableOpacity>
-                      {perChange > 0 ? (
-                        <Text style={[styles.changePositive, styles.change]}>
-                          {perChange.toFixed(2)}%
-                          <Grow
-                            width={18}
-                            height={18}
-                            stroke={"#79C300"}
-                            transform={"translate(0 4)"}
-                          />
-                        </Text>
-                      ) : (
-                        <Text style={[styles.changeNegative, styles.change]}>
-                          {perChange.toFixed(2)}%
-                          <Grow
-                            transform={
-                              "rotate(180) scale(-1, 1) translate(0 -4)"
-                            }
-                            width={18}
-                            height={18}
-                            stroke={"#D64B45"}
-                          />
-                        </Text>
-                      )}
-                    </TouchableOpacity>
-                  </View>
-                </View>
-              );
-            })}
+            <FlatList
+              contentContainerStyle={{ alignSelf: "flex-start" }}
+              numColumns={props.history.length}
+              key={props.history.length}
+              keyExtractor={(item) => item.symbol}
+              data={props.history}
+              renderItem={({ item, index }) => {
+                return renderHistory(item, index);
+              }}
+            />
           </ScrollView>
-        </SafeAreaView>
+        </>
       </View>
     </View>
   );
