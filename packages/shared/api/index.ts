c404efffd82350505110d2bafd4d1e7aa8ee670f
RGTS <mroberson115@gmail.com>
2021-08-13 18:14:15 -0300

    Arrumando dados na Api

--- packages/shared/api/index.ts
+++ packages/shared/api/index.ts
@@ -2,7 +2,7 @@ import Config from 'react-native-config'
 const TOKEN_API = process.env.TOKEN_API || Config.TOKEN_API
 
 export const getInfosSymbol = async (symbol: string) => {
-    const response = await fetch(`https://sandbox.iexapis.com/stable/stock/${symbol}/quote/?token=${TOKEN_API}`, {
+    const response = await fetch(`https://sandbox.iexapis.com/stable/stock/${symbol}/quote/?token=${TOKEN_API}&filter=latestPrice,change,companyName`, {
         method: 'GET',
         headers: {
             Accept: 'application/json',
@@ -21,7 +21,7 @@ export const getInfosSymbol = async (symbol: string) => {
 export const getHistoricalPrices = async (symbol:string) => {
     const dateNow = new Date()
     const date = `${dateNow.getFullYear()}0${dateNow.getMonth() + 1}${dateNow.getDate()}`
-    const response = await fetch(`https://sandbox.iexapis.com/stable/stock/${symbol}/chart/date/${date}?token=${TOKEN_API}`, {
+    const response = await fetch(`https://sandbox.iexapis.com/stable/stock/${symbol}/chart/date/${date}?token=${TOKEN_API}&filter=minute,average,label`, {
         method: 'GET',
         headers: {
             Accept: 'application/json',
@@ -31,7 +31,7 @@ export const getHistoricalPrices = async (symbol:string) => {
     const res = await response.json()
     const arrayData = res.map((value) => {
         return {
-            minute: value.label.includes("PM") ? `0${parseInt(value.minute.slice(0,1)) + 12}:${value.minute.slice(2, 4)}` : value.minute,
+            minute: value.label.includes("PM") ? `${parseInt(value.minute.slice(0,1)) + 12}:${value.minute.slice(3, 5)}` : value.minute,
             average: value.average,
         }
     })
